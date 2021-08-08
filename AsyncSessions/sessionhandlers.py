c495fc40177d7e369e74b97a1567c83ad91115e3
ZackDowning <zackarytdowning@gmail.com>
2021-08-08 14:07:25 -0500

    Separated validation functions and methods

--- AsyncSessions/sessionhandlers.py
+++ AsyncSessions/sessionhandlers.py
@@ -2,10 +2,9 @@ import os
 import sys
 import time
 from concurrent.futures import ThreadPoolExecutor, wait
-from icmplib import ping
 from netmiko import ConnectHandler, ssh_exception, SSHDetect
-from address_validator import ipv4
 from exceptions import TemplatesNotFoundWithinPackage
+from validators import reachability
 
 # Checks for TextFSM templates within single file bundle if code is frozen
 if getattr(sys, 'frozen', False):
@@ -19,35 +18,6 @@ else:
                 os.environ['NET_TEXTFSM'] = './AsyncSessions/templates'
             else:
                 raise TemplatesNotFoundWithinPackage
-
-
-class MgmtIPAddresses:
-    """Input .txt file location containing list of management IP addresses"""
-    def __init__(self, mgmt_file_location):
-        self.mgmt_file_location = mgmt_file_location
-        self.mgmt_ips = []
-        """Formatted set of validated IP addresses"""
-        self.invalid_line_nums = []
-        """Set of invalid line numbers corresponding to line number of management file input"""
-        self.invalid_ip_addresses = []
-        """Set of invalid IP addresses"""
-        self.validate = True
-        """Bool of management IP address file input validation"""
-        with open(self.mgmt_file_location) as file:
-            for idx, address in enumerate(file):
-                ip_address = str(address).strip('\n')
-                if ipv4(ip_address) is False:
-                    self.invalid_line_nums.append(str(idx + 1))
-                    self.invalid_ip_addresses.append(str(address))
-                    self.validate = False
-                    """Bool of management IP address file input validation"""
-                else:
-                    self.mgmt_ips.append(ip_address)
-
-
-def reachability(ip_address, count=4):
-    """Returns bool if host is reachable with default count of 4 pings"""
-    return ping(ip_address, privileged=False, count=count).is_alive
 
 
 class Connection:
